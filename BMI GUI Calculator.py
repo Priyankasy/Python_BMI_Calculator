d6a86bb8e6e00543895a68b1cd9934a4cd0659e7
Priyanka Yadav <101916029+Priyankasy@users.noreply.github.com>
2025-01-17 12:32:03 +0530

    Update BMI GUI Calculator.py

--- BMI GUI Calculator.py
+++ BMI GUI Calculator.py
@@ -39,27 +39,3 @@ result_label.pack()
 
 #Run the GUI Window
 window.mainloop()
-
-
-
-#name = input("Enter your name : ")
-#weight = int(input('Enter the weight :'))
-#height = int(input('Enter the height :'))
-##BMI = weight/(height* height)
-"""print(BMI)
-if BMI >0:
-    if(BMI<18.5):
-        print(name+", You are under weight")
-    elif(BMI<24.9):
-        print(name+", You are normal weight")
-    elif(BMI<29.9):
-        print(name+", You are over weight. You need to exercise more and stop sitting and writing so many pythin tutorials")
-    elif(BMI<34.9):
-        print(name+", You are obese")
-    elif(BMI<39.9):
-        print(name+", You are severely obese")   
-    else:
-        print(name+", You are morbidly obese")
-else:
-    print("Enter a valid inputs")
-    """
